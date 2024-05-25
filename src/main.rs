88bc9c4f8c3af3443a5c8e828661473e8a39b20f
Dmitrii Ovcharenko <ovcharenkodmitry@gmail.com>
2024-05-26 00:38:44 +0300

    - New methods: push_front(), push_back() - check for size and correct index from user - refactored split method - add tests for all methods - add size as util method - new type LinkedListIterator - add imp iter()  who returns LinkedListIterator

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ struct Node {
 struct LinkedList {
     head: Option<Rc<RefCell<Node>>>,
     last: Option<Rc<RefCell<Node>>>,
+}
+
+#[derive(Debug)]
+struct LinkedListIterator {
     current_iter_elem: Option<Rc<RefCell<Node>>>,
 }
 
@@ -20,34 +24,39 @@ impl LinkedList {
         Self {
             head: None,
             last: None,
-            current_iter_elem: None,
         }
     }
 
-    fn add_before_head(&mut self, value: i32) {
-        if self.head.is_none() {
+    fn push_front(&mut self, value: i32) {
+        if let Some(head) = &self.head {
+            self.head = Some(Rc::new(RefCell::new(Node {
+                data: value,
+                next: Some(Rc::clone(head)),
+            })));
+        } else {
             self.head = Some(Rc::new(RefCell::new(Node {
                 data: value,
                 next: None,
             })));
-        } else {
-            let copy_of_head = Rc::clone(self.head.as_ref().unwrap());
-            self.head = Some(Rc::new(RefCell::new(Node {
-                data: value,
-                next: Some(Rc::clone(&copy_of_head)),
-            })));
         }
-        self.current_iter_elem = self.head.clone();
     }
 
-    fn add_after_n(&mut self, value: i32, mut n: i32) {
-        let mut current = self.head.clone();
-
-        while n == 0 {
-            current = current.unwrap().borrow().next.clone();
-            n -= 1;
+    fn add_after_n(&mut self, value: i32, index: i32) -> Result<(), String> {
+        if self.head.is_none() || self.size() < index {
+            return Err(format!("no item with index {}", index));
         }
 
+        if self.size() == index {
+            self.push_back(value);
+            return Ok(());
+        }
+
+        let mut current = self.head.clone();
+        for _ in 0..index {
+            if let Some(node) = current {
+                current = node.borrow().next.clone();
+            }
+        }
         let tmp = current.clone().unwrap();
         let mut ref_mut = tmp.borrow_mut();
         let tmp_next = ref_mut.next.clone();
@@ -55,46 +64,58 @@ impl LinkedList {
         ref_mut.next = Some(Rc::new(RefCell::new(Node {
             data: value,
             next: tmp_next,
-        })))
+        })));
+        Ok(())
     }
 
-    fn add_after_tail(&mut self, value: i32) {
-        if self.head.is_none() {
-            self.head = Some(Rc::new(RefCell::new(Node {
-                data: value,
-                next: None,
-            })));
-            self.current_iter_elem = self.head.clone();
-            self.last = self.head.clone();
+    pub fn size(&self) -> i32 {
+        if let Some(head) = &self.head {
+            let mut count = 1;
+            let mut current = head.clone();
+            while let Some(node) = current.clone().borrow().next.clone() {
+                current = node.clone();
+                count += 1;
+            }
+            return count;
+        }
+        0
+    }
+
+    pub fn push_back(&mut self, value: i32) {
+        let new_rc = Rc::new(RefCell::new(Node {
+            data: value,
+            next: None,
+        }));
+        if let Some(last) = &self.last {
+            last.borrow_mut().next = Some(new_rc.clone());
+            self.last = Some(new_rc.clone());
         } else {
-            let mut current = self.head.clone();
-            while let Some(node) = current {
-                current = node.borrow().next.clone();
-                self.last = Some(node.clone());
-            }
-            self.last.as_ref().unwrap().borrow_mut().next = Some(Rc::new(RefCell::new(Node {
-                data: value,
-                next: None,
-            })));
+            self.head = Some(new_rc.clone());
+            self.last = self.head.clone();
         }
     }
 
-    fn split_after_n(&self, mut n: i32) -> (LinkedList, LinkedList) {
-        let mut fst = LinkedList::new();
-        let mut snd = LinkedList::new();
+    fn split_at_index(self, index: i32) -> (LinkedList, LinkedList) {
+        let mut first_list = LinkedList::new();
+        let mut second_list = LinkedList::new();
 
-        let mut current = self.head.clone();
-        while let Some(node) = current {
-            let x = node.borrow();
-            if n != 0 {
-                n -= 1;
-                fst.add_after_tail(x.data);
+        let mut current_node = self.head;
+        let mut current_index = 0;
+
+        while let Some(node) = current_node {
+            let borrowed_node = node.borrow();
+            let node_data = borrowed_node.data;
+
+            if current_index < index {
+                first_list.push_back(node_data);
             } else {
-                snd.add_after_tail(x.data);
+                second_list.push_back(node_data);
             }
-            current = x.next.clone();
+
+            current_node = borrowed_node.next.clone();
+            current_index += 1;
         }
-        (fst, snd)
+        (first_list, second_list)
     }
 
     fn replace_n_item_with_value(&mut self, mut n: i32, value: i32) {
@@ -109,78 +130,201 @@ impl LinkedList {
             }
         }
     }
+
+    fn iter(&self) -> LinkedListIterator {
+        LinkedListIterator {
+            current_iter_elem: self.head.clone(),
+        }
+    }
 }
 
-impl Iterator for LinkedList {
+impl Iterator for LinkedListIterator {
     type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.current_iter_elem.as_ref()?;
 
-        let rc = self.current_iter_elem.clone().unwrap();
-        let x = rc.borrow();
-        let value = x.data;
-        if x.next.is_some() {
-            self.current_iter_elem = Some(Rc::clone(x.next.as_ref().unwrap()));
+        if let Some(node) = self.current_iter_elem.clone() {
+            self.current_iter_elem = node.borrow().next.clone();
+            Some(node.borrow().data)
         } else {
-            self.current_iter_elem = None;
+            None
         }
-        Some(value)
     }
 }
 
 fn main() {
     //добавлять элемент в начало,
     let mut list = LinkedList::new();
-    list.add_before_head(3);
-    list.add_before_head(2);
-    list.add_before_head(1);
+    list.push_front(3);
+    list.push_front(2);
+    list.push_front(1);
 
-    for x in list {
+    for x in list.iter() {
         println!("{:?}", x);
     }
 
     //добавлять элемент в конец,
     let mut list2 = LinkedList::new();
-    list2.add_after_tail(4);
-    list2.add_after_tail(5);
-    list2.add_after_tail(6);
+    list2.push_back(4);
+    list2.push_back(5);
+    list2.push_back(6);
 
-    for x in list2 {
+    for x in list2.iter() {
         println!("{:?}", x);
     }
 
     // добавлять элемент после N-го
     let mut list3 = LinkedList::new();
-    list3.add_after_tail(7);
-    list3.add_after_tail(9);
-    list3.add_after_n(8, 1);
+    list3.push_back(7);
+    list3.push_back(9);
+    let _ = list3.add_after_n(8, 1);
 
-    for x in list3 {
+    for x in list3.iter() {
         println!("{:?}", x);
     }
 
     // Разделяться на два списка: от начального элемента до- (N-1)-го и от (N-1)-го до последнего.
     let mut list_split = LinkedList::new();
-    list_split.add_after_tail(10);
-    list_split.add_after_tail(11);
-    list_split.add_after_tail(12);
-    list_split.add_after_tail(13);
-    list_split.add_after_tail(14);
-    list_split.add_after_tail(15);
+    list_split.push_back(10);
+    list_split.push_back(11);
+    list_split.push_back(12);
+    list_split.push_back(13);
+    list_split.push_back(14);
+    list_split.push_back(15);
 
-    let (first_part, second_part) = list_split.split_after_n(2);
-    for a in first_part {
+    let (first_part, second_part) = list_split.split_at_index(2);
+    for a in first_part.iter() {
         println!("{:?}", a);
     } // 10 11
 
-    for b in second_part {
+    for b in second_part.iter() {
         println!("{:?}", b);
     } // 12 13 14 15
 
     // Предоставлять возможность изменять элементы списка
     let mut list_edit = LinkedList::new();
-    list_edit.add_after_tail(50);
+    list_edit.push_back(50);
     list_edit.replace_n_item_with_value(0, 100);
     println!("{:?}", list_edit);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::LinkedList;
+
+    #[test]
+    fn iter() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        let mut result = Vec::new();
+        for x in list.iter() {
+            result.push(x);
+        }
+        assert_eq!(vec![1], result);
+    }
+
+    #[test]
+    fn push_front() {
+        let mut list = LinkedList::new();
+        list.push_front(3);
+        list.push_front(2);
+        list.push_front(1);
+        let mut result = Vec::new();
+        for x in list.iter() {
+            result.push(x);
+        }
+        assert_eq!(result, vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn push_back_to_empty() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        assert_eq!(1, list.head.unwrap().borrow().data);
+        assert_eq!(1, list.last.unwrap().borrow().data);
+    }
+
+    #[test]
+    fn push_back() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        list.push_back(4);
+        assert_eq!(4, list.size());
+        assert_eq!(1, list.head.unwrap().borrow().data);
+        assert_eq!(4, list.last.unwrap().borrow().data);
+    }
+
+    #[test]
+    fn size() {
+        assert_eq!(0, LinkedList::new().size());
+
+        let mut list_one = LinkedList::new();
+        list_one.push_back(1);
+        assert_eq!(1, list_one.size());
+
+        let mut list_five = LinkedList::new();
+        list_five.push_back(1);
+        list_five.push_back(2);
+        list_five.push_back(3);
+        list_five.push_back(4);
+        list_five.push_back(5);
+        assert_eq!(5, list_five.size());
+    }
+
+    #[test]
+    fn add_after_n() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(4);
+
+        let result = list.add_after_n(7, 1);
+        assert!(result.is_ok());
+        assert_eq!(4, list.size());
+    }
+
+    #[test]
+    fn add_after_n_with_empty() {
+        let result_for_empty = LinkedList::new().add_after_n(7, 55);
+        assert_eq!("no item with index 55", result_for_empty.err().unwrap());
+
+        let result = LinkedList::new().add_after_n(7, 0);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn add_after_last() {
+        let mut l = LinkedList::new();
+        l.push_back(1);
+        l.push_back(2);
+        let result = l.add_after_n(4, 2);
+        assert!(result.is_ok());
+        assert_eq!(3, l.size());
+    }
+
+    #[test]
+    fn split_after_n() {
+        let mut list_split = LinkedList::new();
+        list_split.push_back(10);
+        list_split.push_back(11);
+        list_split.push_back(12);
+        list_split.push_back(13);
+        list_split.push_back(14);
+        list_split.push_back(15);
+
+        let (fst, snd) = list_split.split_at_index(2);
+        assert_eq!(2, fst.size());
+        assert_eq!(4, snd.size());
+    }
+
+    #[test]
+    fn replace_n_item_with_value() {
+        let mut list_edit = LinkedList::new();
+        list_edit.push_back(50);
+        list_edit.replace_n_item_with_value(0, 100);
+        println!("{:?}", list_edit);
+    }
 }
